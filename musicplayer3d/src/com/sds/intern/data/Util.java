139bee79ba8651d3e95f9701cf560592742563a1
chongmyung <ubuntu@.(none)>
2011-06-15 01:07:52 +0900

    cache관련 파일, timebar 관련 함수나 .java 파일 제거
    camera 관련 파일을 살려둠.
    
    그리드 관련 부분과 디스플레이 관련, 배열, 백그라운드 부분만 남기는 것이 목표
    
    Signed-off-by: chongmyung <ubuntu@.(none)>

---
+++ musicplayer3d/src/com/sds/intern/data/Util.java
@@ -0,0 +1,13 @@
+package com.sds.intern.data;
+
+import android.content.Intent;
+import android.net.Uri;
+
+public class Util {
+
+	public static Intent createSetAsIntent(Uri parse, String mMimeType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ musicplayer3d/src/com/sds/intern/music3d/DisplaySlot.java
@@ -0,0 +1,30 @@
+package com.sds.intern.music3d;
+
+import java.util.HashMap;
+
+import com.sds.intern.data.MediaSet;
+
+public final class DisplaySlot {
+
+	public Texture getTitleImage(
+			HashMap<String, StringTexture> stringTextureTable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasValidLocation() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public MediaSet getMediaSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setMediaSet(MediaSet set) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
